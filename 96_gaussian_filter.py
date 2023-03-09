1d496dc0721333d89241a6eba2768638c0373352
Dongliang <ewang0212@gmail.com>
2023-03-09 22:21:19 +0800

    add gaussian denoising

---
+++ 96_gaussian_filter.py
@@ -0,0 +1,10 @@
+from skimage import io, img_as_float
+from skimage.filters import gaussian
+import cv2
+import numpy as np
+
+
+img = img_as_float(io.imread('./images/BSE_Google_noisy.jpg'))
+img_salt = img_as_float(io.imread('./images/BSE_salt_pepper.jpg'))
+
+

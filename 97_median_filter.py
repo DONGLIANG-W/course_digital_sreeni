8ae181bc6e35a5908c50cf1408734ef9d44a486e
Dongliang <ewang0212@gmail.com>
2023-03-09 23:07:01 +0800

    add and push

---
+++ 97_median_filter.py
@@ -0,0 +1,20 @@
+from skimage import io, img_as_float
+from skimage.filters import gaussian, median
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+img_gaussian = img_as_float(io.imread('./images/BSE_Google_noisy.jpg'))
+img_salt = io.imread('./images/BSE_salt_pepper.jpg', 0)
+
+img = img_salt
+# apply median filter with cv2
+median_cv2 = cv2.medianBlur(img, 3)
+# apply median with skimage
+median_skimage = median(img, mode='constant', cval=0.0)
+cv2.imshow('salt and pepper', img)
+cv2.imshow('skimage median',median_skimage)
+cv2.waitKey()
+cv2.destroyAllWindow()
+
+

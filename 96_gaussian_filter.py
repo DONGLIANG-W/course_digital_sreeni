65413217c686b49e2841f567ed220201501c0af5
Dongliang <ewang0212@gmail.com>
2023-03-09 22:44:37 +0800

    add and push

--- 96_gaussian_filter.py
+++ 96_gaussian_filter.py
@@ -8,6 +8,10 @@ img_gaussian = img_as_float(io.imread('./images/BSE_Google_noisy.jpg'))
 img_salt = img_as_float(io.imread('./images/BSE_salt_pepper.jpg'))
 
 img = img_gaussian
+gaussian_kernel = np.array([[1/16, 1/8, 1/16],
+                            [1/8, 1/4, 1/8],
+                            [1/16, 1/8, 1/16]])
+con_cv2 = cv2.filter2D(img, -1, gaussian_kernel, borderType=cv2.BORDER_CONSTANT)
 
 
 

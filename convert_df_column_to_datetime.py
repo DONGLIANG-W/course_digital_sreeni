deb31b47ceae688175a46b23bc6a810d107ed586
DONGLIANG-W <68606882+DONGLIANG-W@users.noreply.github.com>
2023-07-15 22:04:14 +0800

    Update convert_df_column_to_datetime.py

--- convert_df_column_to_datetime.py
+++ convert_df_column_to_datetime.py
@@ -13,3 +13,20 @@ df = pd.DataFrame(demo_data)
 df['Date'] = pd.to_datetime(df['Date'], format='%m%d%Y').dt.strftime('%d-%m-%Y')
 
 print(df)
+
+
+
+# read excel sheets
+import pandas as pd
+
+# Specify the path to the Excel file
+excel_file_path = 'path/to/your/excel/file.xlsx'
+
+# Read all sheets into a dictionary of dataframes
+dfs = pd.read_excel(excel_file_path, sheet_name=None)
+
+# Access each dataframe by sheet name
+for sheet_name, df in dfs.items():
+    print(f"Sheet: {sheet_name}")
+    print(df)
+    print()  # Empty line for separation
